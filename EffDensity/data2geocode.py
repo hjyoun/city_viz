3fa9e37995f1cde0f8eb1999c76dc68a0c0a772b
Hyejin Youn <visang@gmail.com>
2012-02-09 16:57:12 -0700

    effective density

---
+++ EffDensity/data2geocode.py
@@ -0,0 +1,19 @@
+name2code = {}
+
+for i in open("/Users/visang/Works/EffDensity/EffDensityData/StateNameList.txt"):
+    code, name, dummy = i.split()
+    name2code[name.lower()] = code
+
+fo = open("/Users/visang/Works/EffDensity/EffDensityData/state_alldata.txt")
+fo.readline()
+fm = "{"
+for i in fo:
+    name, nominal, effd1, effd2, effd3 = i.split()[0:5]
+    code = name2code[name]
+    fm = fm + '"'+code+'":'+effd3+','
+
+fm = fm + '}'
+print fm
+
+    
+
